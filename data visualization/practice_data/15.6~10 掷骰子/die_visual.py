f20c4a9c39d95ba0213218631ad2330b97b6a2b8
Cyh-codes <cyh.codes@gmail.com>
2026-05-19 21:52:20 +0800

    掌握骰子,掌握推导式

--- data visualization/practice_data/15.6~10 掷骰子/die_visual.py
+++ data visualization/practice_data/15.6~10 掷骰子/die_visual.py
@@ -7,19 +7,14 @@ die_1 = Die()
 die_2 = Die()
 
 # 掷几次骰子并将结果存储在一个列表中
-results = []
-for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+# 改为列表推导式!!!
+results = [die_1.roll() + die_2.roll() for i in range(1, 1000)]
 
 # 分析结果
-frequencies = []
+# 同样改为列表推导式
 max_result = die_1.num_sides + die_2.num_sides
 poss_results = range(1, max_result+1)
-for value in poss_results:
-    frequency = results.count(value)
-    frequencies.append(frequency) 
-print(frequencies)
+frequencies = [results.count(value) for value in poss_results]
 
 # 对结果进行可视化
 title ='Result of Rolling Two D6 1000 Times'
